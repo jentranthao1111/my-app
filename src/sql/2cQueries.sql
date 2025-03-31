c36c407d6591a77443ac40be35baca1e9e2e1d11
Brian Tran <briankhoitran02@gmail.com>
2025-03-31 13:24:05 -0400

    Altering queries

--- src/sql/2cQueries.sql
+++ src/sql/2cQueries.sql
@@ -10,11 +10,12 @@ FROM booking B
 JOIN customer C ON B.cust_id = C.cust_id
 JOIN room R ON B.room_id = R.room_id;
 
--- Query 3: Count number of bookings per room type (Aggregation)            UNFINISHED
-SELECT R.RoomType, COUNT(B.Booking_ID) AS booking_count
-FROM Booking B
-JOIN Room R ON B.Room_ID = R.Room_ID
-GROUP BY R.RoomType;
+-- Query 3: List all employees working at a specific hotel
+SELECT E.fullname, E.role, H.hotel_name
+FROM Employee E
+JOIN Hotel H ON E.hotel_fid = H.hotel_id
+WHERE H.hotel_name = 'Luxury Stay';
+
 
 -- Query 4: Find details of most recent booking (Nested)
 SELECT *
