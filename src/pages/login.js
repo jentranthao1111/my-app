f1f1a95a8655669764442d429d564bcb53f39d54
jankyxiong <110124590+jankyxiong@users.noreply.github.com>
2025-03-31 22:43:34 -0400

    added login functionality for customer

--- src/pages/login.js
+++ src/pages/login.js
@@ -4,12 +4,39 @@ import './css/login.css';
 const Login = ({ setPage }) => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const handleSubmit = (e, role) => {
+  const handleSubmit = async (e, role) => {
     e.preventDefault(); // Prevent default page reload
 
     if (role === 'user') {
       // authenticate sign in as user
-      // if authentication works, then -> setPage('mainpageuser')
+      try {
+        const response = await fetch('http://localhost:5000/api/customer/validate', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            email, password
+          }),
+        });
+    
+        const data = await response.json();
+        console.log("Server response:", data);
+    
+        if (data.valid) {
+          alert("Customer login successfully!");
+          localStorage.setItem("cust_id", data.cust_id);
+          setPage('mainpageuser');
+        } else {
+          alert("Registration failed: " + (data.message || JSON.stringify(data)));
+        }
+      } catch (error) {
+        console.error("Network or server error:", error);
+        alert("An error occurred. Check console for details.");
+      }
+
+
+     
     } else if (role === 'employee') {
       // authenticate sign in as employee
       // if authentication works, then -> setPage('mainpageemployee')
