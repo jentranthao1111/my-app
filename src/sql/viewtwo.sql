88a4db51d68dff9d2e9389a6b81b2e8c72d41af4
Brian Tran <briankhoitran02@gmail.com>
2025-04-03 23:06:48 -0400

    removing redundant files

--- src/sql/viewtwo.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE VIEW hotel_total_capacity AS
-SELECT h.hotel_id, h.hotel_name, SUM(r.capacity) AS total_capacity
-FROM hotel h
-JOIN room r ON h.hotel_id = r.hotel_id
-GROUP BY h.hotel_id, h.hotel_name;
-
-SELECT * FROM hotel_total_capacity;
